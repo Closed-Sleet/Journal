3b40bfaa096faa1e45b3c6075422ba313a4b8a1d
Sleet <deltasleet@gmail.com>
2023-11-26 21:25:30 -0500

    Organized and minimized.

--- Journal.py
+++ Journal.py
@@ -7,7 +7,7 @@ current_date = datetime.datetime.now(tz=None)
 response = input("Type entry now. \nPress enter when you're done.\n")
 formatted_date = current_date.strftime("%A, %B %d, %Y \n%I:%M%p\n")
 
-if ("good" in response.lower()):
+if (" good " in response.lower()):
 	print("Glad to hear")
 	time.sleep(0.25)
 	print("Let's get some work done\n")
@@ -17,13 +17,9 @@ time.sleep(0.1)
 
 File = input("\nWhere would you like to put this entry?\n")
 
-if File:
-	#print("if File")
-	with open(File +'.txt', 'a') as J:
-		J.write("\n" + formatted_date + "\n" + response + "\n----\n")
-else:
-	#print("else")
+if not File:
 	File = "Journal"
-	with open(File + '.txt', 'a') as J:
-		J.write("\n" + formatted_date + "\n" + response + "\n----\n")
+
+with open(File + '.txt', 'a') as J:
+	J.write("\n" + formatted_date + "\n" + response + "\n----\n")
 print("Entry has been put in " + File + ".txt")
